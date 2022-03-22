cc201e9fbb83a6d9a6d0ad21d40301b7b1c95f63
Max Henneberg <henneberg.maximilian@googlemail.com>
2022-03-22 05:05:46 +0100

    [MaHe]: Test Env

--- src/app/services/database/database.service.ts
+++ src/app/services/database/database.service.ts
@@ -1,21 +1,10 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DatabaseService {
-  // databaseUser: { username: string, password: string };
 
-  constructor(private http: HttpClient) {
-    console.log('TEST:')
-    console.log(process.env.MY_TEST_VAR)
-    // this.databaseUser = {username: '', password: ''};
-    // this.http.get('/assets/database/database_user.json').subscribe(data => {
-    //   console.log(data);
-    //   // @ts-ignore
-    //   this.databaseUser = data;
-    //   console.log(this.databaseUser.username);
-    // });
+  constructor() {
   }
 }
