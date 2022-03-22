dc98bd09c6a6077ab39b5cde609e2c51e6f83dba
Max Henneberg <henneberg.maximilian@googlemail.com>
2022-03-22 04:42:58 +0100

    [MaHe]: Test Env

--- src/app/services/database/database.service.ts
+++ src/app/services/database/database.service.ts
@@ -8,6 +8,7 @@ export class DatabaseService {
   // databaseUser: { username: string, password: string };
 
   constructor(private http: HttpClient) {
+    console.log('USERNAME:')
     console.log(process.env.USERNAME)
     // this.databaseUser = {username: '', password: ''};
     // this.http.get('/assets/database/database_user.json').subscribe(data => {
